bec9c0889721c99026a28b3b56cb40309da1a895
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-09-01 23:56:12 +0100

    loggging

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -22,7 +22,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields) -> None:
+    def __init__(self, fields: List[str]) -> None:
         """ base class"""
         self.field = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
