49c28009e14d64e719897ad2812e941f32a51540
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-09-01 23:01:29 +0100

    loggging

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -31,3 +31,17 @@ class RedactingFormatter(logging.Formatter):
         new_message = filter_datum(self.field, self.REDACTION, record.getMessage(), self.SEPARATOR)
         self.FORMAT = self.FORMAT % {"name": record.name, "levelname":record.levelname, "asctime": f'{time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(record.created))},{int(record.msecs):03d}', "message": new_message}
         return self.FORMAT
+
+
+def get_logger()->logging.Logger:
+    """returns 
+    log """
+    logger = logging.getLogger(name="user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    log = logging.StreamHandler()
+    log.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(log)
+    return logger
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
