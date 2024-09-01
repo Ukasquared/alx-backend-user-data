01766899ae22f1faafb4b02f9658f60f28a4ead7
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-09-01 04:41:35 +0100

    logging modules

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ the log message obfuscated """
 import re
+import logging
+import time
 
 
 def filter_datum(fields, redaction, message, separator):
@@ -9,3 +11,23 @@ def filter_datum(fields, redaction, message, separator):
         message = re.sub(rf"\b{item}[^{separator}]*", 
                     f"{item}={redaction}", message)
     return(message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """ base class"""
+        self.field = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ format log record """
+        new_message = filter_datum(self.field, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        self.FORMAT = self.FORMAT % {"name": record.name, "levelname":record.levelname, "asctime": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(record.created)), "message": new_message}
+        return self.FORMAT
