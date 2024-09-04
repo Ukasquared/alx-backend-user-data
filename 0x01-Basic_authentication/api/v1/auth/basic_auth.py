1dea1c0a7d584ebcba585ad32035f2bf9328b3e8
Judith Ukachukwu <ukachukwujudith95@gmail.com>
2024-09-04 04:58:21 +0100

    decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 Module """
 from api.v1.auth.auth import Auth
 import re
+import base64
+from binascii import Error
 
 
 class BasicAuth(Auth):
@@ -18,3 +20,16 @@ class BasicAuth(Auth):
         if not re.search(r"^Basic\s", authorization_header):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """decode a base64 string"""
+        if not base64_authorization_header:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        decoded = ''
+        try:
+            decoded = base64.b64decode(base64_authorization_header)
+        except Error:
+            return None
+        return decoded.decode('utf-8')
